b75328cfe12f0c48eb2634b8bd0752a7d37ffd3b
4000D <dkdkajej@gmail.com>
2018-04-02 22:06:48 +0900

    test: more scenarios _success_  - holders claim before interval  - holder0 claim after 1 interval  - holder0 claim after 1 more interval  - holder1 claim after 2 intervals  - holder1 claim after 3 intervals
    _other faild scnemarios_
      - holders claim after 2 intervals
      - holders claim after 3 intervals
      - should generate claimed token when transfer occured

--- test/PoS.js
+++ test/PoS.js
@@ -23,33 +23,39 @@ const should = require("chai")
 const Token = artifacts.require("./MiniMeToken.sol");
 const PoS = artifacts.require("./PoS.sol");
 
-contract("PoS", async (accounts) => {
+contract("PoS", async (holders) => {
   let token, pos;
 
   // PoS parameters
-  const posInterval = 100;
-  const posRate = 10;
-  const posCoeff = 100;
-  const posRatio = new BigNumber(1.1); // 10%
+  const posInterval = new BigNumber(100);
+  const posRate = new BigNumber(100);
+  const posCoeff = new BigNumber(1000);
+  const _1ClaimRate = new BigNumber(1.100);
+  const _2ClaimRate = new BigNumber(1.210);
+  const _3ClaimRate = new BigNumber(1.331);
+
   let posInitBlock;
 
   // common parameters
-  const numHolders = 10;
-  const tokenHolders = accounts.slice(1, numHolders + 1);
+  const numHolders = 5;
+  const tokenHolders = holders.slice(1, numHolders + 1);
   const tokenAmount = ether(10);
 
   // helper function
   const moveAfterInterval = async () => {
     const currentBlockNumber = web3.eth.blockNumber;
     const diff = posInterval - (currentBlockNumber - posInitBlock) % posInterval;
-    const targetBlockNumber = currentBlockNumber + diff;
+    const targetBlockNumber = currentBlockNumber + diff + 5; // 5 more blocks
 
-    console.log(`move from ${ web3.eth.blockNumber } to ${ targetBlockNumber } with posInitBlock ${ posInitBlock }`);
+    console.log(`move from ${ web3.eth.blockNumber } to ${ targetBlockNumber } with posInitBlock ${ posInitBlock }, diff ${ diff }`);
 
     await advanceToBlock(targetBlockNumber);
   };
   const getTokenBalance = holder => token.balanceOf(holder);
   const claimTokens = holder => pos.claim(holder, { from: holder });
+  const claimAllHolderTokens = () => Promise.all(tokenHolders
+    .map(claimTokens)
+    .map(p => p.should.be.fulfilled));
 
   // setup
   before(async () => {
@@ -80,12 +86,14 @@ contract("PoS", async (accounts) => {
     await token.changeController(pos.address);
   });
 
-  it("holder cannot claim tokens before interval passed", async () => {
+  it("holders cannot claim tokens before interval passed", async () => {
     const beforeBalances = await Promise.all(tokenHolders.map(getTokenBalance));
 
-    await Promise.all(tokenHolders
-      .map(claimTokens)
-      .map(p => p.should.be.fulfilled));
+    // await Promise.all(tokenHolders
+    //   .map(claimTokens)
+    //   .map(p => p.should.be.fulfilled));
+
+    await claimAllHolderTokens();
 
     const afterBalances = await Promise.all(tokenHolders.map(getTokenBalance));
 
@@ -98,69 +106,140 @@ contract("PoS", async (accounts) => {
     });
   });
 
-  // it("holder can claim tokens after interval passed", async () => {
-  //     await moveAfterInterval();
-  //
-  //     const beforeBalances = await Promise.all(tokenHolders.map(getTokenBalance));
-  //
-  //     await Promise.all(tokenHolders
-  //         .map(claimTokens)
-  //         .map(p => p.should.be.fulfilled));
-  //
-  //     const afterBalances = await Promise.all(tokenHolders.map(getTokenBalance));
-  //
-  //     afterBalances.forEach((afterBalance, i) => {
-  //         const beforeBalance = beforeBalances[i];
-  //
-  //         afterBalance.should.be.bignumber.equal(beforeBalance.mul(posRatio));
-  //     });
-  // });
+  it("a holder can claim tokens after interval passed", async () => {
+    await moveAfterInterval();
 
-  // it("holder can claim tokens after 2 interval passed", async () => {
-  //     let block = web3.eth.blockNumber - posInterval;
-  //     await moveAfterInterval(); // 10%
-  //     await moveAfterInterval(); // 21%
-  //     // await moveAfterInterval(); // 33.1%
-  //
-  //     let currentPosRate = await pos.getClaimRate(block); // 21%
-  //
-  //     let beforeBalances = await Promise.all(accounts.map(getTokenBalance));
-  //
-  //     await Promise.all(accounts
-  //         .map(claimTokens)
-  //         .map(tx => tx.should.be.fulfilled)); // 21%
-  //
-  //     let afterBalances = await Promise.all(accounts.map(getTokenBalance));
-  //
-  //     afterBalances.forEach((afterBalance, i) => {
-  //         const beforeBalance = beforeBalances[i];
-  //         afterBalance.should.be.bignumber
-  //           .equal(beforeBalance.mul(currentPosRate.add(posCoeff)).div(posCoeff));
-  //     });
-  // });
+    const tokenHolder = tokenHolders[ 0 ];
+    const claimRate = _1ClaimRate;
 
-  it("should call claim method when onTransfer method called", async () => {
-    const account0BalanceBefore = await getTokenBalance(accounts[ 0 ]);
-    const account1BalanceBefore = await getTokenBalance(accounts[ 1 ]);
+    const beforeBalance = await getTokenBalance(tokenHolder);
+    await claimTokens(tokenHolder);
+    const expectedBalance = beforeBalance.mul(claimRate);
 
-    const account2BalanceBefore = await getTokenBalance(accounts[ 2 ]);
-    const account3BalanceBefore = await getTokenBalance(accounts[ 3 ]);
+    const afterBalance = await getTokenBalance(tokenHolder);
 
-    await moveAfterInterval(); // 10%
-
-    await pos.onTransfer(accounts[ 0 ], accounts[ 1 ], 100);
-
-    const account0BalanceAfter = await getTokenBalance(accounts[ 0 ]);
-    const account1BalanceAfter = await getTokenBalance(accounts[ 1 ]);
-    const account2BalanceAfter = await getTokenBalance(accounts[ 2 ]);
-    const account3BalanceAfter = await getTokenBalance(accounts[ 3 ]);
-
-    account0BalanceAfter.should.be.bignumber
-      .equal(account0BalanceBefore.mul((posRate + posCoeff) / posCoeff));
-    account1BalanceAfter.should.be.bignumber
-      .equal(account1BalanceBefore.mul((posRate + posCoeff) / posCoeff));
-
-    account2BalanceAfter.should.be.bignumber.equal(account2BalanceBefore);
-    account3BalanceAfter.should.be.bignumber.equal(account3BalanceBefore);
+    afterBalance.should.be.bignumber.equal(expectedBalance);
   });
+
+  it("a holder can claim tokens after one more interval passed", async () => {
+    await moveAfterInterval();
+
+    const tokenHolder = tokenHolders[ 0 ];
+    const claimRate = _1ClaimRate;
+
+    const beforeBalance = await getTokenBalance(tokenHolder);
+    await claimTokens(tokenHolder);
+    const expectedBalance = beforeBalance.mul(claimRate);
+
+    const afterBalance = await getTokenBalance(tokenHolder);
+
+    afterBalance.should.be.bignumber.equal(expectedBalance);
+  });
+
+  it("a holder can claim tokens after 2 intervals passed", async () => {
+    const tokenHolder = tokenHolders[ 1 ];
+    const claimRate = _2ClaimRate;
+
+    const beforeBalance = await getTokenBalance(tokenHolder);
+    await claimTokens(tokenHolder);
+    const expectedBalance = beforeBalance.mul(claimRate);
+
+    const afterBalance = await getTokenBalance(tokenHolder);
+
+    afterBalance.should.be.bignumber.equal(expectedBalance);
+  });
+
+  it("a holder can claim tokens after 3 intervals passed", async () => {
+    await moveAfterInterval();
+
+    const tokenHolder = tokenHolders[ 2 ];
+    const claimRate = _3ClaimRate;
+
+    const beforeBalance = await getTokenBalance(tokenHolder);
+    await claimTokens(tokenHolder);
+    const expectedBalance = beforeBalance.mul(claimRate);
+
+    const afterBalance = await getTokenBalance(tokenHolder);
+
+    afterBalance.should.be.bignumber.equal(expectedBalance);
+  });
+
+  // TODO: fix test or contract
+  it("holders can claim tokens after 2 intervals passed", async () => {
+    await claimAllHolderTokens();
+    await moveAfterInterval();
+    await moveAfterInterval();
+
+    const beforeBalances = await Promise.all(tokenHolders.map(getTokenBalance));
+    const claimRate = _2ClaimRate;
+
+    await Promise.all(tokenHolders
+      .map(claimTokens)
+      .map(p => p.should.be.fulfilled));
+
+    const afterBalances = await Promise.all(tokenHolders.map(getTokenBalance));
+
+    afterBalances.forEach((afterBalance, i) => {
+      const beforeBalance = beforeBalances[ i ];
+
+      afterBalance.should.be.bignumber
+        .equal(beforeBalance.mul(claimRate));
+    });
+  });
+
+  // TODO: fix test or contract
+  it("holders can claim tokens after 3 interval passed", async () => {
+    await claimAllHolderTokens();
+    await moveAfterInterval(); // 10%
+    await moveAfterInterval(); // 21%
+    await moveAfterInterval(); // 33.1%
+
+    const beforeBalances = await Promise.all(holders.map(getTokenBalance));
+    const claimRate = _3ClaimRate;
+
+    await Promise.all(holders
+      .map(claimTokens)
+      .map(p => p.should.be.fulfilled));
+
+    const afterBalances = await Promise.all(holders.map(getTokenBalance));
+
+    afterBalances.forEach((afterBalance, i) => {
+      const beforeBalance = beforeBalances[ i ];
+      afterBalance.should.be.bignumber
+        .equal(beforeBalance.mul(claimRate));
+    });
+  });
+
+  // TODO: fix test or contract
+  it("should generate claimed token when transfer occured", async () => {
+    await claimAllHolderTokens();
+    await moveAfterInterval(); // 10%
+    await moveAfterInterval(); // 21%
+
+    const holder0 = holders[ 0 ];
+    const holder1 = holders[ 1 ];
+    const tokenAmountToTransfer = ether(0.001);
+
+    const holder0BalanceBefore = await getTokenBalance(holder0);
+    const holder1BalanceBefore = await getTokenBalance(holder1);
+    const claimRate = _2ClaimRate;
+
+    const expectedHolder0Balance = holder0BalanceBefore.mul(claimRate)
+      .sub(tokenAmountToTransfer);
+    const expectedHolder1Balance = holder1BalanceBefore.mul(claimRate)
+      .add(tokenAmountToTransfer);
+
+    await token.transfer(holder1, tokenAmountToTransfer, { from: holder0 });
+
+    const holder0BalanceAfter = await getTokenBalance(holder0);
+    const holder1BalanceAfter = await getTokenBalance(holder1);
+
+    holder0BalanceAfter.should.be.bignumber
+      .equal(expectedHolder0Balance);
+    holder1BalanceAfter.should.be.bignumber
+      .equal(expectedHolder1Balance);
+  });
+
+  // TODO: test when multiple claims or transfers is in a single block
+  // especially for one single holder
 });
