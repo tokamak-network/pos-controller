95a57bbbf7ba63510c8d6bb63bef78605aebf555
4000D <dkdkajej@gmail.com>
2018-04-18 16:17:32 +0900

    contracts: inherit ERC20 as base parent to resolve linearization

--- contracts/POSMintableToken.sol
+++ contracts/POSMintableToken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.18;
 
+import "./zeppelin/token/ERC20/ERC20.sol";
 import "./zeppelin/token/MintableToken.sol";
 import "./POSTokenAPI.sol";
 
 
-contract POSMintableToken is MintableToken, POSMintableTokenAPI {
-    function POSMintableToken() public {}
+contract POSMintableToken is ERC20, MintableToken, POSMintableTokenAPI {
 }
